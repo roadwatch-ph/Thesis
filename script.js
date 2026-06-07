4476c325e69bdcbb4db7e60b6ab35e8e7b5811e8
projectroadwatch-ph <projectroadwatch23@gmail.com>
2026-06-07 23:04:30 +0800

    Update Apps Script web app URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxl_K3tapX4Zn4GiyNvh9-O_nrb7PfvZQJYZvdfs9-urFYu1TF92D80_E2RyvDIzscN/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzmrLsaG7MLnbK0ZhxwxBOQ-dHURPTZTG3Ae7MAeYG5RpnW-IqFlo7nqR0NWGkaGru/exec";
 const CLIENT_VERSION = "contribution-settings-v2";
 const EXPECTED_BACKEND_VERSION = "contribution-settings-v2";
 const MAX_FILE_SIZE = 5 * 1024 * 1024;
